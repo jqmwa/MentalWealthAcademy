769b278983047e2adf1db20e4e53fc1e43656a1c
Jhinnbay <jquinnmarsh@gmail.com>
2025-12-10 06:49:02 -0500

    Description and embed image

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,28 @@ import '../styles/globals.css';
 
 export const metadata: Metadata = {
   title: 'Mental Wealth Academy',
-  description: 'A pedagogical educational toolbox for small IRL communities looking to safely digitize their community and assets.',
+  description: 'Mental Wealth Academy is a virtual learning platform for the next generation. It\'s an online school where students can take classes, complete quests, earn rewards, and learn together. We use blockchain and cryptocurrency to make learning more engaging and rewarding.',
   icons: {
     icon: '/icons/favicon.png',
+  },
+  openGraph: {
+    title: 'Mental Wealth Academy',
+    description: 'Mental Wealth Academy is a virtual learning platform for the next generation. It\'s an online school where students can take classes, complete quests, earn rewards, and learn together. We use blockchain and cryptocurrency to make learning more engaging and rewarding.',
+    images: [
+      {
+        url: '/icons/embbedBanner.png',
+        width: 1200,
+        height: 630,
+        alt: 'Mental Wealth Academy - Next Gen Micro-University',
+      },
+    ],
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Mental Wealth Academy',
+    description: 'Mental Wealth Academy is a virtual learning platform for the next generation. It\'s an online school where students can take classes, complete quests, earn rewards, and learn together. We use blockchain and cryptocurrency to make learning more engaging and rewarding.',
+    images: ['/icons/embbedBanner.png'],
   },
 };
 
